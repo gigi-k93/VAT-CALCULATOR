adbb1284cbcdfb73028932ab87883f167582fdd7
Gloria Kyei <67934038+gigi-k93@users.noreply.github.com>
2022-07-04 10:21:59 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package vat_calculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ Vat myv=new Vat();
+ myv.createAndShowGUI();
+	}
+
+}
